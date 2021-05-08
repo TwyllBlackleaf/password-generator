4afda398ba8ab56b0c7e8615d7cfb9efeff6fd1e
Twyll <twyllblackleaf@gmail.com>
2021-05-08 16:11:26 -0400

    formatting tweaks

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 // Assignment code here
+var generatePassword = function () {
 
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 
 }
 
