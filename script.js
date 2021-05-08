b1b4bfac124d20db0aa78d1e1ad2bc7373056025
Twyll <twyllblackleaf@gmail.com>
2021-05-08 17:43:39 -0400

    error checking for parameters input

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Assignment code here
 var generatePassword = function () {
+    // Function for getting password length
     var getLength = function () {
         passwordLength = prompt("Please enter a length for your password. Minimum of 8 characters, maximum of 128 characters.");
         passwordLength = parseInt(passwordLength);
@@ -20,21 +21,38 @@ var generatePassword = function () {
         }
 
         return passwordLength;
-    }
-    
+    };
+
+    // Function for getting parameters for password
+    var getParameters = function() {
+        var useLowerCase = confirm("Include lowercase characters?");
+        var useUpperCase = confirm("Include uppercase characters?");
+        var useNumeric = confirm("Include numeric characters?");
+        var useSpecial = confirm("Include special characters?");
+
+        var parametersObj = {
+            lowerCase: useLowerCase,
+            upperCase: useUpperCase,
+            numeric: useNumeric,
+            special: useSpecial,
+            total: useLowerCase + useUpperCase + useNumeric + useSpecial
+        }
+
+        return parametersObj;  
+    };
+  
     // Get parameters for password
     var length = getLength();
-    var lowerCase = confirm("Include lowercase characters?");
-    var upperCase = confirm("Include uppercase characters?");
-    var numeric = confirm("Include numeric characters?");
-    var special = confirm("Include special characters?");
+    var parameters = getParameters();
+
+    // Check that at least one character type has been selected
+    while (parameters.total === 0) {
+        alert("Password must have at least one type of character.");
+        parameters = getParameters();
+    }
 
     console.log(length);
-    console.log(lowerCase);
-    console.log(upperCase);
-    console.log(numeric);
-    console.log(special);
-
+    console.log(parameters);
 }
 
 // Get references to the #generate element
